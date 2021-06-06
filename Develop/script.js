c6f52ce8425765cc091a1be519b7ae47db17f74f
algmoreno <alg.moreno00@gmail.com>
2021-06-06 15:14:44 -0700

    local storage progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,130 +1,147 @@
-var todaysDate = function() {
-    var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    $("#currentDay").text(currentDate); 
-}
+$(document).ready(function () {
 
-var hourList = [
-    {
-        id: "0",
-        hour: "9:00",
-        time: "09",
-        meridiem: "am",
-        toDo: ""
-    },
-    {
-        id: "1",
-        hour: "10:00",
-        time: "10",
-        meridiem: "am",
-        toDo: ""
-    },
-    {
-        id: "2",
-        hour: "11:00",
-        time: "11",
-        meridiem: "am",
-        toDo: ""
-    },
-    {
-        id: "3",
-        hour: "12:00",
-        time: "12",
-        meridiem: "pm",
-        toDo: ""
-    },
-    {
-        id: "4",
-        hour: "1:00",
-        time: "13",
-        meridiem: "pm",
-        toDo: ""
-    },
-    {
-        id: "5",
-        hour: "2:00",
-        time: "14",
-        meridiem: "pm",
-        toDo: ""
-    },
-    {
-        id: "6",
-        hour: "3:00",
-        time: "15",
-        meridiem: "pm",
-        toDo: ""
-    },
-    {
-        id: "7",
-        hour: "4:00",
-        time: "16",
-        meridiem: "pm",
-        toDo: ""
-    },
-    {
-        id: "8",
-        hour: "5:00",
-        time: "17",
-        meridiem: "pm",
-        toDo: ""
-    },
+    var todaysDate = function() {
+        var currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+        $("#currentDay").text(currentDate); 
+    }
     
-]
-
-hourList.forEach(function(thisHour) {
-
-    var timeRow = $("<form>").attr({
-        "class": "row"
-    })
-    $(".container").append(timeRow); 
-
-    var hourEl = $("<div>") 
-    .text(`${thisHour.hour}${thisHour.meridiem}`)
-    .attr({
-        "class": "col-md-1 hour"
-    })
-
-    var reminderArea = $("<div>")
-    .attr({
-        "class": "col-md-9"
-    })
-    console.log(moment().format("HH"))
-        if (thisHour.time < moment().format("HH")) {
-            reminderArea.addClass("past")
-        }
-        else if (thisHour.time === moment().format("HH")) {
-            reminderArea.addClass("present")
-        }
-        else if (thisHour.time > moment().format("HH")){
-            reminderArea.addClass("future")
-        }
+    var hourList = [
+        {
+            id: "0",
+            hour: "9:00",
+            time: "09",
+            meridiem: "am",
+            toDo: ""
+        },
+        {
+            id: "1",
+            hour: "10:00",
+            time: "10",
+            meridiem: "am",
+            toDo: ""
+        },
+        {
+            id: "2",
+            hour: "11:00",
+            time: "11",
+            meridiem: "am",
+            toDo: ""
+        },
+        {
+            id: "3",
+            hour: "12:00",
+            time: "12",
+            meridiem: "pm",
+            toDo: ""
+        },
+        {
+            id: "4",
+            hour: "1:00",
+            time: "13",
+            meridiem: "pm",
+            toDo: ""
+        },
+        {
+            id: "5",
+            hour: "2:00",
+            time: "14",
+            meridiem: "pm",
+            toDo: ""
+        },
+        {
+            id: "6",
+            hour: "3:00",
+            time: "15",
+            meridiem: "pm",
+            toDo: ""
+        },
+        {
+            id: "7",
+            hour: "4:00",
+            time: "16",
+            meridiem: "pm",
+            toDo: ""
+        },
+        {
+            id: "8",
+            hour: "5:00",
+            time: "17",
+            meridiem: "pm",
+            toDo: ""
+        },
         
-    var reminderText = $("<textarea>")
-    .attr({
-        "class": "col-md-12 reminder-text"
-    })
-    reminderArea.append(reminderText)
+    ]
 
-    var saveArea = $("<div>")
-    .attr({
-        "class": "col-md-2 saveArea"
-    })
 
-    var saveButton = $("<button>")
-    .attr({
-        "class": "btn btn-outline-warning saveBtn"
-    })
-
-    var addSign = $("<span>")
-    .attr({
-        "class": "oi oi-plus"
-    })
+    hourList.forEach(function(thisHour) {
     
-    saveButton.append(addSign)
-    saveArea.append(saveButton)
+        var timeRow = $("<form>").attr({
+            "class": "row"
+        })
+        $(".container").append(timeRow); 
+    
+        var hourEl = $("<div>") 
+        .text(`${thisHour.hour}${thisHour.meridiem}`)
+        .attr({
+            "class": "col-md-1 hour"
+        })
+    
+        var reminderArea = $("<div>")
+        .attr({
+            "class": "col-md-9"
+        })
+        // console.log(moment().format("HH"))
+            if (thisHour.time < moment().format("HH")) {
+                reminderArea.addClass("past")
+            }
+            else if (thisHour.time === moment().format("HH")) {
+                reminderArea.addClass("present")
+            }
+            else if (thisHour.time > moment().format("HH")){
+                reminderArea.addClass("future")
+            }
+            
+        var reminderText = $("<textarea>")
+        .attr({
+            "class": "col-md-12 reminder-text",
+            "id": `HELLO ${thisHour.time}`
+        })
+        reminderArea.append(reminderText)
+    
+        var saveArea = $("<div>")
+        .attr({
+            "class": "col-md-2 saveArea"
+        })
+    
+        var saveButton = $("<button>")
+        .attr({
+            "id": "saveBtn",
+            "class": "btn btn-outline-warning saveBtn"
+        })
+
+        var addSign = $("<span>")
+        .attr({
+            "class": "oi oi-plus"
+        })
+
+        $("#saveBtn").on("click", function(event){
+            event.preventDefault()
+            var setValue = $(this).parent().siblings().children(".reminder-text").val();
+            console.log(setValue);
+            var setTime = $(this).parent().siblings().children(".reminder-text").attr("id");
+            console.log(setTime);
+            localStorage.setItem(setValue, setTime); 
+        })
         
+        
+        saveButton.append(addSign)
+        saveArea.append(saveButton)
+            
+    
+        timeRow.append(hourEl, reminderArea, saveArea);
+        $("#hour .reminder-text").val(localStorage.getItem("hour"))
+    })
 
-    timeRow.append(hourEl, reminderArea, saveArea);
-
+    
+    todaysDate(); 
 })
-
-todaysDate(); 
